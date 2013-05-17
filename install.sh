53adb6f88d2161e07424312ebf43706c054e74b8
Thom Hastings <tghastings@gmail.com>
2013-05-17 02:42:25 +0000

    fix

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ ln -s $WORKINGDIR/.profile ~/.profile
 ln -s $WORKINGDIR/.screen ~/.screen
 ln -s ~/.screen/.screenrc ~/.screenrc
 ln -s $WORKINGDIR/.vim ~/.vim
-ln -s ~/.vim/.vimrc ~/.vim
-git config --global color.ui true
+ln -s ~/.vim/.vimrc ~/.vimrc
+git config --global --replace-all color.ui true
